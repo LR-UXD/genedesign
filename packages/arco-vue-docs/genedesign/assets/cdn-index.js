23827e001e09d59e657eed371e868f1cb068f213
“Dear-peng” <1922289754@qq.com>
2025-04-01 14:21:39 +0800

    first commit

---
+++ packages/arco-vue-docs/genedesign/assets/cdn-index.js
@@ -0,0 +1,8 @@
+window.arcoConfig = {
+    internalHostName: "arco.bytedance.net",
+    externalHostName: "arco.design",
+    internalUnpkgUrl: "https://sf-unpkg-src.bytedance.net",
+    externalUnpkgUrl: "https://unpkg.com",
+    bnpmRegistry: "https://bnpm.byted.org",
+    npmRegistry: "https://registry.npmjs.org",
+};
